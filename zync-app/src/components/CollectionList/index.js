982f44f53e92044020007a2ed8d54c2049529f71
Paul <pfvatterott@gmail.com>
2022-04-24 13:15:03 -0600

    grades toggle added

--- zync-app/src/components/CollectionList/index.js
+++ zync-app/src/components/CollectionList/index.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from "react";
-import { Collection, CollectionItem } from 'react-materialize'
+import { Collection, CollectionItem, Icon } from 'react-materialize'
 import FilterInput from "../FilterInput";
 import "./style.css"
 
 export default function CollectionList(props) {
     const [userInfo, setUserInfo] = useState([])
     const [nameFilter, setNameFilter] = useState('')
+    const [forceUpdate, setForceUpdate] = useState(0)
 
     useEffect(() => {
         console.log(props.userData)
+        let tempUserArray = props.userData
+        tempUserArray.forEach(user => {
+            if (user.areGradesHidden === null) {
+                user.areGradesHidden = true
+            }
+        });
         if(nameFilter === '') {
-            findAverages(props.userData)
+            findAverages(tempUserArray)
         }
         else {
-            filterResults(props.userData)
+            filterResults(tempUserArray)
         }
     }, [props.userData, nameFilter])
 
@@ -42,12 +49,28 @@ export default function CollectionList(props) {
         findAverages(filteredUsers)
     }
 
+    function handleShowGrades(id) {
+        let tempUserInfo = userInfo
+        for (let i = 0; i < tempUserInfo.length; i++) {
+            if (tempUserInfo[i].id === id && tempUserInfo[i].areGradesHidden === true) {
+                tempUserInfo[i].areGradesHidden = false
+            }
+            else if (tempUserInfo[i].id === id && tempUserInfo[i].areGradesHidden === false) {
+                tempUserInfo[i].areGradesHidden = true
+            }
+        }
+        console.log(tempUserInfo)
+        setUserInfo(tempUserInfo)
+        setForceUpdate(forceUpdate + 1)
+    }
+
     return (
         <div>
             <Collection>
                 <FilterInput nameFilter={nameFilter} setNameFilter={(x) => handleSetNameFilter(x)}/>
                 {userInfo.length > 0 ? userInfo.map(user => (
                     <CollectionItem className="avatar" key={user.id}>
+                        <Icon className="right addRemoveButton" onClick={(x) => handleShowGrades(user.id)}>{user.areGradesHidden ? "add" : "remove"}</Icon>
                         <img className="userImage left" src={user.pic} />
                         <span className="title userName">{user.firstName} {user.lastName}</span>
                         <div className="indent">
@@ -55,6 +78,11 @@ export default function CollectionList(props) {
                             <p>Company: {user.company}</p>
                             <p>Skill: {user.skill}</p>
                             <p>Average: {user.average}%</p>
+                        </div>
+                        <div className={(user.areGradesHidden ? "gradesHidden grades" : "gradesNotHidden grades")} id={user.id}>
+                            {user.grades.map(grade => (
+                                <p>Test 1: {grade}%</p>
+                            ))}
                         </div>
                     </CollectionItem>
                 )): null}
